28378d26d29215b2546630ef84c269e8f164193b
Valera <108022376+kekobka@users.noreply.github.com>
2024-06-29 02:46:03 +0700

    1.0.0v

--- src/grpc.rs
+++ src/grpc.rs
@@ -1,58 +1,164 @@
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::{ SystemTime, UNIX_EPOCH };
 
-use discord_rich_presence::{activity, DiscordIpc, DiscordIpcClient};
+use discord_rich_presence::{ activity, DiscordIpc, DiscordIpcClient };
+use lua::luaL_checkstring;
 use once_cell::sync::Lazy;
-use rglua::lua::{lua_call, lua_getfield, lua_getglobal, lua_tointeger};
 use rglua::*;
-use rglua::{interface, lua::LuaState};
-static mut TIMESTAMP: i64 = 0;
-static mut CLIENT: Lazy<DiscordIpcClient> =
-    Lazy::new(|| DiscordIpcClient::new("1122498317755101195").expect("Failed to create client"));
+use rglua::{ interface, lua::LuaState };
 
-fn get_player_count(l: LuaState) -> i64 {
-    lua_getglobal(l, cstr!("player"));
-    lua_getfield(l, -1, cstr!("GetCount"));
-    lua_call(l, 0, 1);
-    lua_tointeger(l, -1).try_into().unwrap()
-}
-unsafe fn update_internal(player_count: String) {
-    let activity = activity::Activity::new()
-        .state(&player_count)
+// че за хуйня??
+static mut TIMESTAMP: i64 = 0;
+static mut LARGE_IMAGE: &str = "https://gmodmbox.online/meteor.png";
+static mut LARGE_TEXT: &str = "METEOR SANDBOX";
+static mut SMALL_IMAGE: &str = "https://developer.valvesoftware.com/w/images/e/ea/Gmod_icon.png";
+static mut SMALL_TEXT: &str = "Garry's mod";
+static mut STATE: &str = "empty";
+static mut BUTTON1_LABEL: &str = "Подключиться!";
+static mut BUTTON1_URL: &str = "steam://connect/89.111.141.237:27015";
+static mut BUTTON2_LABEL: &str = "Дискорд";
+static mut BUTTON2_URL: &str = "https://discord.com/invite/5zxJ5XvpRP";
+
+static mut CLIENT: Lazy<DiscordIpcClient> = Lazy::new(||
+    DiscordIpcClient::new("1027974934498123777").expect("Failed to create client")
+);
+
+unsafe fn update_internal() {
+    let activity = activity::Activity
+        ::new()
+        .state(STATE)
         .assets(
-            activity::Assets::new()
-                .large_image("https://gmodmbox.online/box_motorola.png")
-                .large_text("METEOR SANDBOX")
-                .small_image("https://developer.valvesoftware.com/w/images/e/ea/Gmod_icon.png")
-                .small_text("Garry's mod"),
+            activity::Assets
+                ::new()
+                .large_image(LARGE_IMAGE)
+                .large_text(LARGE_TEXT)
+                .small_image(SMALL_IMAGE)
+                .small_text(SMALL_TEXT)
         )
-        .buttons(vec![
-            activity::Button::new("Подключиться!", "https://gmodmbox.online/connect"),
-            activity::Button::new("Дискорд", "https://discord.com/invite/5zxJ5XvpRP"),
-        ])
+        .buttons(
+            vec![
+                activity::Button::new(BUTTON1_LABEL, BUTTON1_URL),
+                activity::Button::new(BUTTON2_LABEL, BUTTON2_URL)
+            ]
+        )
         .timestamps(activity::Timestamps::new().start(TIMESTAMP));
-    let _ = CLIENT.set_activity(activity);
+    println!("update");
+    println!("{}", LARGE_IMAGE);
+    if CLIENT.set_activity(activity).is_err() {
+        println!("Reconnecting grpc...");
+        CLIENT.reconnect().unwrap();
+    }
 }
+
 #[lua_function]
-pub(crate) fn update(l: LuaState) -> Result<i32, interface::Error> {
-    let playerscount: i64 = get_player_count(l);
-    let from: String = playerscount.to_string().to_owned() + " / 128";
-    unsafe { update_internal(from) };
+pub(crate) fn set_large_image(l: LuaState) -> Result<i32, interface::Error> {
+    unsafe {
+        LARGE_IMAGE = rstr!(luaL_checkstring(l, 1));
+    }
     Ok(0)
 }
-pub(crate) fn init(l: LuaState) {
+
+#[lua_function]
+pub(crate) fn set_large_text(l: LuaState) -> Result<i32, interface::Error> {
     unsafe {
-        TIMESTAMP = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_secs() as i64
-    };
-    match unsafe { CLIENT.connect() } {
-        Ok(_) => update(l),
-        Err(_) => {
-            println!("Client failed to connect to Discord, Please try again or relaunch Discord.");
-            0
+        LARGE_TEXT = rstr!(luaL_checkstring(l, 1));
+    }
+    Ok(0)
+}
+
+#[lua_function]
+pub(crate) fn set_small_image(l: LuaState) -> Result<i32, interface::Error> {
+    unsafe {
+        SMALL_IMAGE = rstr!(luaL_checkstring(l, 1));
+    }
+    Ok(0)
+}
+
+#[lua_function]
+pub(crate) fn set_small_text(l: LuaState) -> Result<i32, interface::Error> {
+    unsafe {
+        SMALL_TEXT = rstr!(luaL_checkstring(l, 1));
+    }
+    Ok(0)
+}
+
+#[lua_function]
+pub(crate) fn set_state(l: LuaState) -> Result<i32, interface::Error> {
+    unsafe {
+        STATE = rstr!(luaL_checkstring(l, 1));
+    }
+    Ok(0)
+}
+
+#[lua_function]
+pub(crate) fn set_button_1(l: LuaState) -> Result<i32, interface::Error> {
+    unsafe {
+        BUTTON1_LABEL = rstr!(luaL_checkstring(l, 1));
+        BUTTON1_URL = rstr!(luaL_checkstring(l, 2));
+    }
+    Ok(0)
+}
+
+#[lua_function]
+pub(crate) fn set_button_2(l: LuaState) -> Result<i32, interface::Error> {
+    unsafe {
+        BUTTON2_LABEL = rstr!(luaL_checkstring(l, 1));
+        BUTTON2_URL = rstr!(luaL_checkstring(l, 2));
+    }
+    Ok(0)
+}
+
+#[lua_function]
+pub(crate) fn set_client_id(l: LuaState) -> Result<i32, interface::Error> {
+    unsafe {
+        CLIENT.client_id = rstr!(luaL_checkstring(l, 1)).to_owned();
+    }
+    Ok(0)
+}
+
+#[lua_function]
+pub(crate) fn close(_l: LuaState) -> Result<i32, interface::Error> {
+    unsafe {
+        let _ = CLIENT.close();
+    }
+    Ok(0)
+}
+
+#[lua_function]
+pub(crate) fn reconnect(_l: LuaState) -> Result<i32, interface::Error> {
+    unsafe {
+        let _ = CLIENT.reconnect();
+    }
+    Ok(0)
+}
+#[lua_function]
+pub(crate) fn connect_grpc(l: LuaState) -> Result<i32, interface::Error> {
+    unsafe {
+        match CLIENT.connect() {
+            Ok(_) => printgm!(l, "grpc connected"),
+            Err(_) => {
+                printgm!(
+                    l,
+                    "Client failed to connect to Discord, Please try again or relaunch Discord."
+                );
+                0;
+            }
         }
-    };
+    }
+    Ok(0)
+}
+
+#[lua_function]
+pub(crate) fn update(_l: LuaState) -> Result<i32, interface::Error> {
+    unsafe {
+        update_internal();
+    }
+    Ok(0)
+}
+
+pub(crate) fn init(_l: LuaState) {
+    unsafe {
+        TIMESTAMP = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() as i64;
+    }
 }
 
 pub(crate) fn deinit() {
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,24 @@
 pub mod grpc;
 
-use rglua::{interface, prelude::*};
+use rglua::{ interface, prelude::* };
 
 #[gmod_open]
 fn main(l: LuaState) -> Result<i32, interface::Error> {
     grpc::init(l);
-    let lib = reg! [
-        "update" => grpc::update
+    let lib =
+        reg![
+            "setLargeImage" => grpc::set_large_image,
+            "setLargeText" => grpc::set_large_text,
+            "setSmallImage" => grpc::set_small_image,
+            "setSmallText" => grpc::set_small_text,
+            "setState" => grpc::set_state,
+            "setButton1" => grpc::set_button_1,
+            "setButton2" => grpc::set_button_2,
+            "setClientID" => grpc::set_client_id,
+            "close" => grpc::close,
+            "reconnect" => grpc::reconnect,
+            "connect" => grpc::connect_grpc,
+            "update" => grpc::update
     ];
 
     luaL_register(l, cstr!("grpc"), lib.as_ptr());
